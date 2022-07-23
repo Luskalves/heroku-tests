c5eb232187099bf1f1e3329851ab609e690bf91f
Luskalves <lusca.alves32@gmail.com>
2022-07-23 10:06:09 -0300

    testando

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.APP_PORT;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Criando os baguio');
+});
+
+app.listen(PORT, () => {
+  console.log(`Ouvindo na porta ${PORT}`)
+})
